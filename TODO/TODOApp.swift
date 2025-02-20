c5a635e70f00fe0b70dd2f27c3d5e7b083f1efe6
MuathOmarieh <muath.omarieh@gmail.com>
2025-02-20 20:21:43 +0300

    Initial Commit

---
+++ TODO/TODOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOApp.swift
+//  TODO
+//
+//  Created by Muath Omarieh on 20/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct TODOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TODOTests/TODOTests.swift
@@ -0,0 +1,17 @@
+//
+//  TODOTests.swift
+//  TODOTests
+//
+//  Created by Muath Omarieh on 20/02/2025.
+//
+
+import Testing
+@testable import TODO
+
+struct TODOTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
